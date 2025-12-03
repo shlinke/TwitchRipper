9002e13e5952b549b1b675b21f9e4b28d800ea5d
Ryan <ryanraymond2010@gmail.com>
2025-12-03 00:26:16 +0000

    Code Skeleton

--- TwitchRipper.py
+++ TwitchRipper.py
@@ -2,21 +2,21 @@ import requests
 import subprocess
 import datetime
 import time
+
 channel = ""
 Token = ""
 outputFolder = ""
 streamlink = ""
 
-while True:
-    headers = {
+headers = {
     "Authorization": f"Bearer {Token}",
     "Client-Id": ""
 }
+user_id = requests.get(f"https://api.twitch.tv/helix/users?login={channel}", headers=headers).json()["data"][0]["id"]
 
-url = f"https://api.twitch.tv/helix/streams?user_login={channel}"
-response = requests.get(url, headers=headers).json()
-if response.get("data"):
-    dateLong = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    
-    subprocess.run([streamlink])
-time.sleep(1)
+while True:
+    response = requests.get(f"https://api.twitch.tv/helix/streams?user_login={channel}", headers=headers).json()
+    if (response.get("data")):
+        dateLong = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        subprocess.run([streamlink])
+    time.sleep(1)
