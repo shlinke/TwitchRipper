3550097cf481492f28e49678fab470db9de592c0
Ryan <ryanraymond2010@gmail.com>
2025-12-03 00:26:16 +0000

    Basic Code Base

--- TwitchRipper.py
+++ TwitchRipper.py
@@ -1,4 +1,22 @@
-channel = ""
-bearer_token = ""
-output_folder = ""
+import requests
+import subprocess
+import datetime
+import time
+channel = "narpy"
+Token = ""
+outputFolder = ""
 streamlink = ""
+
+while True:
+    headers = {
+    "Authorization": f"Bearer {Token}",
+    "Client-Id": ""
+}
+
+url = f"https://api.twitch.tv/helix/streams?user_login={channel}"
+response = requests.get(url, headers=headers).json()
+if response.get("data"):
+    dateLong = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    
+    subprocess.run([streamlink])
+time.sleep(1)
